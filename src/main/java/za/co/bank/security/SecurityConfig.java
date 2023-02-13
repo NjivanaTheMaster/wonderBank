5f39efa49591632115e581486d78d6dff42d83e3
Njabulo Lukhele <njabuloelukhele@gmail.com>
2023-02-13 16:29:40 +0200

    Added security configuration layers

---
+++ src/main/java/za/co/bank/security/SecurityConfig.java
@@ -0,0 +1,53 @@
+package za.co.bank.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableMethodSecurity //enable annotation-based security.
+// enables @PreAuthorize, @PostAuthorize, @PreFilter, and @PostFilter by default.
+public class SecurityConfig {
+
+    private UserDetailsService userDetailsService;
+
+    public SecurityConfig(UserDetailsService userDetailsService){
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    public static PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(); //We are using the Spring security provided BCryptPasswordEncoder class to encrypt the passwords
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf().disable()
+                .authorizeHttpRequests((authorize) ->
+                        //authorize.anyRequest().authenticated()
+                        authorize.requestMatchers(HttpMethod.GET, "/api/**").permitAll() //We are allowing anyone to access login REST API with security configuration
+                                .requestMatchers("/api/auth/**").permitAll()
+                                .anyRequest().authenticated()
+
+                );
+
+        return http.build();
+    }
+
+}
